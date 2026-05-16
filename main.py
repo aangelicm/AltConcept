822e3a77961a30ae26ae68290c8c3e48245e3b7c
aangelicm <makves193@icloud.com>
2026-05-16 20:21:52 +0300

    доделала голую базу подо все

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from database import init_db  # <-- Теперь здесь init_db
-import os
+from database import init_db  
+import os 
+
+from routers.authorization import router as authorization_router
+from routers.buyer import router as buyer_router
+from routers.seller import router as seller_router
+
 
 app = FastAPI()
 
+# Подключаем папку static (картинки, css, js)
+if os.path.exists("static"):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(authorization_router)
+app.include_router(buyer_router)
+app.include_router(seller_router)
+
+
 @app.on_event("startup")
 def startup_event():
-    init_db() # <-- Вызываем нашу новую функцию
+    init_db()
 
-# ... остальной код main.py ...
+@app.get("/")
+def root():
+    return {"message": "AltConcept API работает. Перейди на /auth/register или /auth/login"}
+
+
+
 
 
 
