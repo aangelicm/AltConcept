06bd5e5ee7883519e9b30771b44538df2fa63751
aangelicm <makves193@icloud.com>
2026-05-18 11:46:11 +0300

    исправила часть бэка

--- routers/authorization.py
+++ routers/authorization.py
@@ -1,18 +1,3 @@
-# from fastapi import FastAPI, Form
-# import sqlite3
-# #создаю приложение фаст апи
-
-# app = FastAPI()
-# @app.post("/redister") #отправляю данные на сервер 
-
-# def redister(username: str = Form(...), password: str = Form(...)):
-#     connection = sqlite3.connect("shpp.bd")
-#     cursor = connection.cursor()
-
-#     cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
-#     connection.commit()
-#     return {"message": "все крута"}
-
 from fastapi import APIRouter, Depends, HTTPException, Response, Cookie, Request
 from fastapi.templating import Jinja2Templates
 from passlib.context import CryptContext
